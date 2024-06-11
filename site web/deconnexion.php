a4ccbc5fb7ce99dc4d7d98f09031bbbac8872cf8
romaisimO <simard.romain.iut@gmail.com>
2024-06-11 16:34:54 +0200

    Update deconnexion.php

--- site web/deconnexion.php
+++ site web/deconnexion.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
-	session_destroy();
-	header('Location:connexion.php')
+    session_start(); // Démarrage de la session / Start the session
+    session_destroy(); // Destruction de la session / Destroy the session
+    header('Location:connexion.php'); // Redirection vers la page de connexion / Redirect to the login page
 ?>
