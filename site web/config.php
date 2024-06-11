4f0c38604411cb37814af2724699abbea70fc613
romaisimO <simard.romain.iut@gmail.com>
2024-06-11 15:17:37 +0200

    Update config.php

--- site web/config.php
+++ site web/config.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost"; // MySQL server name
-$username = "root"; // MySQL username
-$password = "passroot"; // MySQL password
-$dbname = "sae23"; // Database name
+$servername = "localhost"; // Nom du serveur MySQL // MySQL server name
+$username = "root"; // Nom d'utilisateur MySQL // MySQL username
+$password = "passroot"; // Mot de passe MySQL // MySQL password
+$dbname = "sae23"; // Nom de la base de données // Database name
 
-// Connect to the database
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Connexion à la base de données // Connect to the database
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Échec de la connexion : " . $conn->connect_error);
+// Vérifier la connexion // Check the connection
+if (!$conn) {
+    die("Échec de la connexion : " . mysqli_connect_error()); // Affiche un message d'erreur si la connexion échoue // Display an error message if the connection fails
 }
 ?>
