f56d6ce198c2df1a8366fa1f70deafb9a15e8ebb
Remidubuc <remi.dubuc.iut@gmail.com>
2024-06-15 15:07:08 +0200

    com en anglais

--- site web/deconnexion.php
+++ site web/deconnexion.php
@@ -1,5 +1,5 @@
 <?php
-    session_start(); // Démarrage de la session / Start the session
-    session_destroy(); // Destruction de la session / Destroy the session
-    header('Location:connexion.php'); // Redirection vers la page de connexion / Redirect to the login page
+    session_start(); // Start the session
+    session_destroy(); // Destroy the session
+    header('Location:connexion.php'); // Redirect to the login page
 ?>
