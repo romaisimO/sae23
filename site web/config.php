b89732c45b7b887e82f5ecf2c8256db61218e229
Remidubuc <remi.dubuc.iut@gmail.com>
2024-06-15 15:05:51 +0200

    com en anglais

--- site web/config.php
+++ site web/config.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost"; // Nom du serveur MySQL // MySQL server name
-$username = "root"; // Nom d'utilisateur MySQL // MySQL username
-$password = "passroot"; // Mot de passe MySQL // MySQL password
-$dbname = "sae23"; // Nom de la base de données // Database name
+$servername = "localhost"; // MySQL server name
+$username = "root"; // MySQL username
+$password = "passroot"; // MySQL password
+$dbname = "sae23"; // Database name
 
-// Connexion à la base de données // Connect to the database
+// Connect to the database
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Vérifier la connexion // Check the connection
+// Check the connection
 if (!$conn) {
-    die("Échec de la connexion : " . mysqli_connect_error()); // Affiche un message d'erreur si la connexion échoue // Display an error message if the connection fails
+    die("Échec de la connexion : " . mysqli_connect_error()); // Display an error message if the connection fails
 }
 ?>
